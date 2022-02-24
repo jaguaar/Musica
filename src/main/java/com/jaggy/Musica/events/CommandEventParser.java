f34e957fda327aa397205c633a5cecf32870ac4a
tim.witters <tim.witters@corilus.be>
2022-02-24 13:51:51 +0100

    Shortcuts

--- src/main/java/com/jaggy/Musica/events/CommandEventParser.java
+++ src/main/java/com/jaggy/Musica/events/CommandEventParser.java
@@ -1,38 +1,72 @@
 package com.jaggy.Musica.events;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-
 import net.dv8tion.jda.api.entities.Message;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.Properties;
+
 @Component
 public class CommandEventParser {
-	private String PREFIX;
 
-	public CommandEventParser(@Value("${command.prefix}") String PREFIX) {
-		this.PREFIX = PREFIX;
-	}
+    private final String PREFIX;
 
-	public Optional<CommandEvent> parseCommandEvent(final Message message) {
-		if (theAuthorIsNotABot(message) && startsWithPrefix(message)) {
-			final String contentRaw = message.getContentRaw();
-			final String[] split = contentRaw.split("\\s");
-			return switch (split.length) {
-				case 1 -> Optional.of(new CommandEvent(message, split[0].substring(PREFIX.length()), List.of()));
-				default -> Optional.of(new CommandEvent(message, split[0].substring(PREFIX.length()), Arrays.asList(split).subList(1, split.length)));
-			};
-		}
-		return Optional.empty();
-	}
+    private final Properties shortcutList = new Properties();
 
-	private boolean startsWithPrefix(final Message message) {
-		return message.getContentRaw().startsWith(PREFIX);
-	}
+    public CommandEventParser(@Value("${command.prefix}") final String PREFIX) {
+        this.PREFIX = PREFIX;
+        loadShortcutList();
+    }
 
-	private boolean theAuthorIsNotABot(final Message message){
-		return !message.getAuthor().isBot();
-	}
+    public Optional<CommandEvent> parseCommandEvent(final Message message) {
+        if (theAuthorIsNotABot(message)) {
+            if (startsWithPrefix(message)) {
+                final String contentRaw = message.getContentRaw();
+                final String[] split = contentRaw.split("\\s");
+                return switch (split.length) {
+                    case 1 -> Optional.of(new CommandEvent(message, split[0].substring(PREFIX.length()), List.of()));
+                    default -> Optional.of(new CommandEvent(message, split[0].substring(PREFIX.length()), Arrays.asList(split).subList(1, split.length)));
+                };
+            } else if (isSpotifyLink(message)) {
+                return Optional.of(new CommandEvent(message, "play", List.of(message.getContentRaw())));
+            } else if (isShortcutPlaylistQueue(message)) {
+                return Optional.of(new CommandEvent(message, "play", List.of(shortcutList.getProperty(message.getContentRaw()))));
+            }
+        }
+        return Optional.empty();
+    }
+
+    private boolean isSpotifyLink(final Message message) {
+        return message.getContentRaw().matches("^https:\\/\\/open.spotify.com[\\S]*\\Z");
+    }
+
+    private boolean isShortcutPlaylistQueue(final Message message) {
+        return shortcutList.containsKey(message.getContentRaw());
+    }
+
+    private boolean startsWithPrefix(final Message message) {
+        return message.getContentRaw().startsWith(PREFIX);
+    }
+
+    private boolean theAuthorIsNotABot(final Message message) {
+        return !message.getAuthor().isBot();
+    }
+
+    private void loadShortcutList() {
+        try (final InputStream input = CommandEventParser.class.getClassLoader().getResourceAsStream("shortcut.properties")) {
+            shortcutList.load(input);
+        } catch (final IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected Properties getShortcutList() {
+        return shortcutList;
+    }
+
 }
---
+++ src/test/java/com/jaggy/Musica/events/CommandEventParserTest.java
@@ -0,0 +1,74 @@
+package com.jaggy.Musica.events;
+
+import net.dv8tion.jda.internal.entities.ReceivedMessage;
+import net.dv8tion.jda.internal.entities.SelfUserImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class CommandEventParserTest {
+
+    private CommandEventParser parse;
+    private ReceivedMessage receivedMessage;
+
+    @BeforeEach
+    public void setUp() {
+        parse = new CommandEventParser(".");
+        receivedMessage = mock(ReceivedMessage.class);
+    }
+
+    @Test
+    void playYoutube() {
+        mockMessage(".p youtube.com");
+        final CommandEvent event = parse.parseCommandEvent(receivedMessage).orElseThrow();
+
+        assertThat(event.getAction()).isEqualTo("p");
+        assertThat(event.getArgs().get(0)).isEqualTo("youtube.com");
+    }
+
+    @Test
+    void playCommand() {
+        mockMessage(".play command");
+        final CommandEvent event = parse.parseCommandEvent(receivedMessage).orElseThrow();
+
+        assertThat(event.getAction()).isEqualTo("play");
+        assertThat(event.getArgs().get(0)).isEqualTo("command");
+    }
+
+    @Test
+    void playSpotifyLink() {
+        final String spotifyLink = "https://open.spotify.com/playlist/37i9dQZF1DXauOWFg72pbl?si=f3bcf7d855274a61";
+        mockMessage(spotifyLink);
+        final CommandEvent event = parse.parseCommandEvent(receivedMessage).orElseThrow();
+
+        assertThat(event.getAction()).isEqualTo("play");
+        assertThat(event.getArgs().get(0)).isEqualTo(spotifyLink);
+    }
+
+    @Test
+    void playShortcutBeer() {
+        testShortcut("beer");
+    }
+
+    @Test
+    void playShortcutBunker() {
+        testShortcut("bunker");
+    }
+
+    private void testShortcut(final String beer) {
+        final String command = beer;
+        mockMessage(command);
+        final CommandEvent event = parse.parseCommandEvent(receivedMessage).orElseThrow();
+
+        assertThat(event.getAction()).isEqualTo("play");
+        assertThat(event.getArgs().get(0)).isEqualTo(parse.getShortcutList().get(command));
+    }
+
+    private void mockMessage(final String message) {
+        when(receivedMessage.getContentRaw()).thenReturn(message);
+        when(receivedMessage.getAuthor()).thenReturn(new SelfUserImpl(1l, null));
+    }
+}
