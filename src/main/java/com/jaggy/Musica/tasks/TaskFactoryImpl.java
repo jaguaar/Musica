2eb17ea76f3754a237e4763563ea9a61e037b111
tim.witters <tim.witters@corilus.be>
2022-03-03 17:15:46 +0100

    Shuffle PR

--- src/main/java/com/jaggy/Musica/tasks/TaskFactoryImpl.java
+++ src/main/java/com/jaggy/Musica/tasks/TaskFactoryImpl.java
@@ -1,16 +1,11 @@
 package com.jaggy.Musica.tasks;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 import com.jaggy.Musica.JaggyBot;
 import com.jaggy.Musica.events.CommandEvent;
-import com.jaggy.Musica.tasks.audio.ClearTask;
-import com.jaggy.Musica.tasks.audio.PlayTask;
-import com.jaggy.Musica.tasks.audio.QueueTask;
-import com.jaggy.Musica.tasks.audio.ShuffleTask;
-import com.jaggy.Musica.tasks.audio.SkipTask;
+import com.jaggy.Musica.tasks.audio.*;
 import com.jaggy.Musica.tasks.messaging.InsultTask;
 
 @Component
@@ -26,22 +21,15 @@ public class TaskFactoryImpl implements TaskFactory {
 	public Task buildTask(final CommandEvent commandEvent) {
 		return switch (commandEvent.getAction()) {
 		case "play", "p" -> new PlayTask(bot, commandEvent.getMessage(), commandEvent.getArgs(), false, false);
+		case "playshuffle", "ps" -> new PlayTask(bot, commandEvent.getMessage(), commandEvent.getArgs(), false, true);
 		case "playnext", "pn" -> new PlayTask(bot, commandEvent.getMessage(), commandEvent.getArgs(), true, false);
+		case "playnextshuffle", "pns" -> new PlayTask(bot, commandEvent.getMessage(), commandEvent.getArgs(), true, true);
 		case "skip", "s" -> new SkipTask(bot, commandEvent.getMessage());
 		case "queue", "q" -> new QueueTask(bot, commandEvent.getMessage());
-		case "shuffle" -> createShuffleOrPlayTask(commandEvent);
+		case "shuffle" -> new ShuffleTask(bot, commandEvent.getMessage());
 		case "clear" -> new ClearTask(bot, commandEvent.getMessage());
 		case "bravo" -> new InsultTask(bot, commandEvent.getMessage());
 		default -> new UnknownTask(commandEvent.getMessage());
 		};
-	}
-
-	@NotNull
-	private ShuffleTask createShuffleOrPlayTask(CommandEvent commandEvent) {
-		if(!commandEvent.getArgs().isEmpty()) {
-			new PlayTask(bot, commandEvent.getMessage(), commandEvent.getArgs(), false, true);
-		}
-
-		return new ShuffleTask(bot, commandEvent.getMessage());
 	}
 }
