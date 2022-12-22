4d0afbcf39835071d13050e663e76dd1f31831ef
tim.witters <tim.witters@corilus.be>
2022-12-22 14:29:48 +0100

    Volume Adjustment

--- src/main/java/com/jaggy/Musica/handlers/SoundHandler.java
+++ src/main/java/com/jaggy/Musica/handlers/SoundHandler.java
@@ -1,35 +1,38 @@
 package com.jaggy.Musica.handlers;
 
+import java.nio.ByteBuffer;
+
+import org.jetbrains.annotations.Nullable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
 import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.player.DefaultAudioPlayerManager;
 import com.sedmelluq.discord.lavaplayer.track.playback.MutableAudioFrame;
-import net.dv8tion.jda.api.audio.AudioSendHandler;
-import org.jetbrains.annotations.Nullable;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-import java.nio.ByteBuffer;
+import net.dv8tion.jda.api.audio.AudioSendHandler;
 
 @Component
 public class SoundHandler implements AudioSendHandler {
 	private final AudioPlayer audioPlayer;
 	private final AudioPlayerManager manager;
 	private final TrackScheduler trackScheduler;
-	private ByteBuffer buffer;
+	private final ByteBuffer buffer;
 	private final MutableAudioFrame frame;
 
 	@Autowired
-	public SoundHandler() {
-		manager = new DefaultAudioPlayerManager();
+	public SoundHandler(@Value("${discord.volume}") final int volume) {
+		this.manager = new DefaultAudioPlayerManager();
 		this.audioPlayer = manager.createPlayer();
+		this.audioPlayer.setVolume(volume);
 
-		buffer = ByteBuffer.allocate(1024);
+		this.buffer = ByteBuffer.allocate(1024);
 		this.frame = new MutableAudioFrame();
 
-
-		trackScheduler = new TrackScheduler(audioPlayer);
-		audioPlayer.addListener(trackScheduler);
+		this.trackScheduler = new TrackScheduler(audioPlayer);
+		this.audioPlayer.addListener(trackScheduler);
 
 		this.frame.setBuffer(buffer);
 	}
